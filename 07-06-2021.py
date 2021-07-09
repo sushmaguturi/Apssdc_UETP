d9771999f57d4142f30ea299bd4eb4e21706f2fa
venugopal74124 <87061378+venugopal74124@users.noreply.github.com>
2021-07-09 12:25:04 +0530

    Add files via upload

---
+++ 07-06-2021.py
@@ -0,0 +1,40 @@
+
+#This is a program for addition
+"""
+hello
+i am Venugopal
+from APSSDC
+
+a=2
+b=3
+c=a+b
+print(c)"""
+                        #Escape Sequences(\)
+
+#print("hi welcome to \npython class by APSSDC" )
+#print("hi welcome to \tpython class by APSSDC" )
+#print("hi welcome to\"python class by APSSDC" )
+
+#vEnu=10
+#print(venu)
+#print(help("keywords"))
+#False=10
+#print(False)
+
+"""a=2#integer
+b=2.5#Float
+c=2+3j#complex
+d=True#Bool
+e=None
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
+
+print(a,id(a))
+print(b,id(b))
+print(c,id(c))
+print(d,id(d))
+print(e,id(e))
+"""
